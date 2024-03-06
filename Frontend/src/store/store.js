293ca59aa3f3fd74b02d84ad5f25add642f4199e
manoj <pitlamanoj1@gmail.com>
2024-03-07 01:07:34 +0530

    Frontend setup & redux toolkit

---
+++ Frontend/src/store/store.js
@@ -0,0 +1,11 @@
+import  { configureStore } from '@reduxjs/toolkit'
+import authSlice from "./authSlice"
+import SidebarSlice from "./SidebarSlice"
+const store = configureStore({
+  reducer:{
+     auth: authSlice,
+     sidebar: SidebarSlice
+  }
+})
+
+export default store
