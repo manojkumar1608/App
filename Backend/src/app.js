68f98765fdf56afe7b846435c484537725f77cfb
manoj <pitlamanoj1@gmail.com>
2024-05-06 16:54:02 +0530

    cleanup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,6 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import bodyParser from "body-parser"
 const app = express()
 
 
@@ -14,7 +13,6 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use(bodyParser.json())
 
 
 //routes import
